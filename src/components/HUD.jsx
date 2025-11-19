453a0ca8b20a93ad365ff40bf68949a51f213e96
KVG24 <141866132+KVG24@users.noreply.github.com>
2025-11-19 11:39:13 +0000

    Fix png icons referencing in HUD component
    Change folder structure
    Add public folder and move all png icons there

--- src/components/HUD.jsx
+++ src/components/HUD.jsx
@@ -19,7 +19,7 @@ export default function HUD({ characters, foundCharacters, time }) {
                             return (
                                 <CharacterDiv key={index} $found={isFound}>
                                     <img
-                                        src={`src/assets/${character.img}`}
+                                        src={`/${character.img}`}
                                         alt={character.name}
                                     />
                                     <p>{character.name}</p>
